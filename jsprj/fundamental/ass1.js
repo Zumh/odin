5131e99a0b1b43407b98fd16cb74f0ebe3cd6dce
Zumh <17211423+Zumh@users.noreply.github.com>
2022-12-08 15:41:09 -0500

    fundamental assignment 1 is done

---
+++ jsprj/fundamental/ass1.js
@@ -0,0 +1,39 @@
+//Add 2 numbers together! (just type console.log(23 + 97) into your html file)
+console.log(23 + 97)
+
+//Add a sequence of 6 different numbers together.
+console.log(1+2+3+4+5+6+7+8+9)
+
+//Print the solution to the following equation: (4 + 6 + 9) / 77
+console.log((4+6+9) / 77)
+
+//Answer should be approximately 0.24675
+
+//Let’s use variables!
+//Type this statement at the top of the script tag: let a = 10
+//In the console console.log(a) should print 10
+let a = 10;
+console.log(a)
+//Try the following in the console: 9 * a
+console.log(a * 9)
+//and this: let b = 7 * a (returns undefined *) and then console.log(b)
+let b = 7 * a;
+console.log(b)
+
+//You should be getting the hang of this by now… try this sequence:
+//Declare a constant variable max with the value 57
+const MAX = 57;
+
+//Set another variable actual to max - 13
+let actual = 0;
+actual = MAX - 13;
+
+//Set another variable percentage to actual / max
+let percentage = 0;
+percentage = actual / MAX; 
+
+//If you type percentage in the console and press enter you should see a value like 0.7719
+console.log(actual)
+console.log(percentage)
+
+//Take a few minutes to keep playing around with various things in your script tag. Eventually, we will learn how to actually make those numbers and things show up on the webpage, but all of this logic will remain the same, so make sure you’re comfortable with it before moving on.
